22fe41d4182017920d6865ccd6b4e45e4dc72af3
insun4210@gmail.com <insun4210@gmail.com>
2024-10-25 16:18:32 +0900

    no message

--- src/main/java/com/pgm/jpademo2/service/BoardServiceImpl.java
+++ src/main/java/com/pgm/jpademo2/service/BoardServiceImpl.java
@@ -25,18 +25,24 @@ public class BoardServiceImpl implements BoardService{
     private final ModelMapper modelMapper;
     private final BoardRepository boardRepository;
 
+
     @Override
     public Long register(BoardDTO boardDTO) {
-        Board board = modelMapper.map(boardDTO, Board.class);
+        //Board board = modelMapper.map(boardDTO, Board.class);
+        Board board = dtoToEntity(boardDTO);
         Long bno = boardRepository.save(board).getBno();
         return bno;
     }
 
     @Override
     public BoardDTO readOne(Long bno) {
-        Optional<Board> result = boardRepository.findById(bno);
+        //Optional<Board> result = boardRepository.findById(bno);
+        Optional<Board> result = boardRepository.findByIdWithImages(bno);
+
         Board board = result.orElseThrow();
-        BoardDTO boardDTO = modelMapper.map(board, BoardDTO.class);
+
+        //BoardDTO boardDTO = modelMapper.map(board, BoardDTO.class);
+        BoardDTO boardDTO= entityToDTO(board);
         return boardDTO;
     }
 
@@ -44,7 +50,16 @@ public class BoardServiceImpl implements BoardService{
     public void modify(BoardDTO boardDTO) {
         Optional<Board> result = boardRepository.findById(boardDTO.getBno());
         Board board = result.orElseThrow();
+
         board.change(boardDTO.getTitle(), boardDTO.getContent());
+        board.clearImages();
+        if(boardDTO.getFileNames() != null) {
+            for (String fileName : boardDTO.getFileNames()) {
+                String[] arr=fileName.split("_");
+                board.addImage(arr[0], arr[1]);
+            }
+        }
+
         boardRepository.save(board);
 
     }
@@ -52,7 +67,6 @@ public class BoardServiceImpl implements BoardService{
     @Override
     public void remove(Long bno) {
         boardRepository.deleteById(bno);
-
     }
 
     @Override
@@ -60,19 +74,18 @@ public class BoardServiceImpl implements BoardService{
         String[] types = pageRequestDTO.getTypes();
         String keyword = pageRequestDTO.getKeyword();
         Pageable pageable = pageRequestDTO.getPageable("bno");
-
-        Page<Board> result=null;
-       // Page<Board> result = boardRepository.searchAll(types,keyword, pageable);
+        Page<Board> result = boardRepository.searchAll(types,keyword, pageable);
+        //Page<Board> result=null;
         //Page<Board> result = boardRepository.searchAll(keyword, pageable);
-        if(types==null|| types.length<1) {
-            result = boardRepository.findAll(pageable);
-        }else{
-            result = boardRepository.searchAll(keyword, pageable);
-        }
+//        if(types==null|| types.length<1) {
+//            result = boardRepository.findAll(pageable);
+//        }else{
+//            result = boardRepository.searchAll(keyword, pageable);
+//        }
 
         List<BoardDTO> dtoList = result.getContent().stream()
-                .map(board -> modelMapper.map(board,BoardDTO.class)).collect(Collectors.toList());
-
+                .map(board -> modelMapper.map(board,BoardDTO.class))
+                .collect(Collectors.toList());
 
         return PageResponseDTO.<BoardDTO>withAll()
                 .pageRequestDTO(pageRequestDTO)
